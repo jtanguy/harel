51772ae008f0146da853bd61d56d1a450ad3bd75
Julien Tanguy <julien.tanguy@jhome.fr>
2015-09-03 12:16:34 +0200

    Add random short generation

--- Harel.hs
+++ Harel.hs
@@ -1,5 +1,19 @@
 module Main where
 
+import Control.Monad (replicateM)
+import qualified Database.Redis as Redis
+import System.Random (randomRIO)
+
+import Servant
+
+alphabet = ['a'..'z']++['A'..'Z']++['0'..'9']
+chooseFrom alph = (alph !!) <$> randomRIO (0,length alph -1)
+
+genShort = replicateM 7 (chooseFrom alphabet)
+
 
 main :: IO ()
-main = putStrLn "Hello world ! This is Harel"
+main = do
+  putStrLn "Hello world ! This is Harel"
+  putStr "Here is a short id: "
+  genShort >>= putStrLn
--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, hedis, servant, stdenv }:
+{ mkDerivation, base, hedis, random, servant, servant-lucid
+, servant-server, stdenv
+}:
 mkDerivation {
   pname = "harel";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hedis servant ];
+  executableHaskellDepends = [
+    base hedis random servant servant-lucid servant-server
+  ];
   homepage = "http://github.com/jtanguy/harel";
   description = "Haskell + Redis Url shortener powered by servant)";
   license = stdenv.lib.licenses.bsd3;
