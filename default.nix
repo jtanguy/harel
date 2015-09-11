76826c4313a293fa3e986834179cff823aade78d
Julien Tanguy <julien.tanguy@jhome.fr>
2015-09-11 23:29:54 +0200

    Bump version to 0.2

--- default.nix
+++ default.nix
@@ -1,16 +1,15 @@
 { mkDerivation, aeson, base, bytestring, either, hedis, mtl
-, network-uri, random, servant, servant-lucid, servant-server
-, stdenv, text, warp
+, network-uri, random, servant, servant-server, stdenv, text, warp
 }:
 mkDerivation {
   pname = "harel";
-  version = "0.1.0.0";
+  version = "0.2.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson base bytestring either hedis mtl network-uri random servant
-    servant-lucid servant-server text warp
+    servant-server text warp
   ];
   homepage = "http://github.com/jtanguy/harel";
   description = "Haskell + Redis Url shortener powered by servant)";
